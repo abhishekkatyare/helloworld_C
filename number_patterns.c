25df75b11539da9a285ae668b4beb9859917dab3
Abhishek Katyare <abhishekkatyare@users.noreply.github.com>
2020-11-08 09:28:49 +0530

    Update number_patterns.c

--- number_patterns.c
+++ number_patterns.c
@@ -21,6 +21,31 @@ int main() {
         printf("\n");
         count = count + 1 + rows - k;
     }
+    
+    // Enter you limit: 5
+               
+    //             10 
+    //          10 20 10 
+    //       10 20 30 20 10 
+    //    10 20 30 40 30 20 10 
+    // 10 20 30 40 50 40 30 20 10 
+    printf("Enter you limit: ");
+    scanf("%d", &rows);
+    for(i=0;i<=rows;i++)
+    {
+     /* print blank spaces */
+        for(j=1;j<=rows-i;j++)
+	        printf("   ");
+     /* Display number in ascending order upto middle*/
+        for(j=1;j<=i;j++)
+            printf("%d ",j*10);
+ 
+     /* Display  number in reverse order after middle */
+        for(j=i-1;j>=1;j--)
+	        printf("%d ",j*10);
+ 
+        printf("\n");
+    }
 
     return 0;
 }
